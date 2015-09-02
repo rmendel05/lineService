c36b1d4ecfd38c0d899833bb48ed1ab31ae6267d
rmendel05 <rmcommerce@gmail.com>
2015-09-02 12:50:31 -0400

    Initial simple design

---
+++ lineServer/src/main/java/com/rmendel/lineManager/service/LineResponseFilter.java
@@ -0,0 +1,23 @@
+package com.rmendel.lineManager.service;
+
+import javax.ws.rs.ext.Provider;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+import com.sun.jersey.spi.resource.Singleton;
+
+@Singleton
+@Provider
+public class LineResponseFilter implements ContainerResponseFilter {
+	 
+	@Override
+	public ContainerResponse filter(
+			ContainerRequest request,
+			ContainerResponse response) {
+
+		// Support x-site access for use with Swagger and other tools
+		response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+		return response;
+	}
+}
